11f143abf3bfb09bb7c69333891e296be22e99d5
Gertjan Reynaert <gertjan@typework.com>
2014-08-09 23:21:35 +0200

    Initial Commit

---
+++ Binairo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Binairo
+//
+//  Created by Gertjan Reynaert on 9/08/14.
+//  Copyright (c) 2014 Gertjan Reynaert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
